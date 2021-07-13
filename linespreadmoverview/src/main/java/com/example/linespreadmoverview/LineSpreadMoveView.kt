5de712c7beee0a89d294fd2e35bfc7aa63c52a07
Anwesh43 <anweshthecool0@gmail.com>
2021-07-14 01:59:27 +0530

    adding Canvas extension methods

--- linespreadmoverview/src/main/java/com/example/linespreadmoverview/LineSpreadMoveView.kt
+++ linespreadmoverview/src/main/java/com/example/linespreadmoverview/LineSpreadMoveView.kt
@@ -29,3 +29,29 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawLineSpreadMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * (1 - sc1))
+    for (j in 0..2) {
+        save()
+        rotate(deg * (1f - j) * sc2)
+        translate(0f, -(h / 2) * sc3)
+        drawLine(0f, 0f, 0f, -size, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLSMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSpreadMove(scale, w, h, paint)
+}
+
