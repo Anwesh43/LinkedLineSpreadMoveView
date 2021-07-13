6ddc99e602fe540a2269686afcc8133670f3d7f5
Anwesh43 <anweshthecool0@gmail.com>
2021-07-14 02:12:09 +0530

    adding LSMNode class

--- linespreadmoverview/src/main/java/com/example/linespreadmoverview/LineSpreadMoveView.kt
+++ linespreadmoverview/src/main/java/com/example/linespreadmoverview/LineSpreadMoveView.kt
@@ -118,4 +118,45 @@ class LineSpreadMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSMNode(var i : Int, val state : State = State()) {
+
+        private var next : LSMNode? = null
+        private var prev : LSMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSMNode {
+            var curr : LSMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
