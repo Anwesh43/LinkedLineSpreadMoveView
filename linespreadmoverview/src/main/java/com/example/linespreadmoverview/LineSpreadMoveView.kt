35513124eecde0bd3317ff0e3a87c1267aacdcef
Anwesh43 <anweshthecool0@gmail.com>
2021-07-14 02:01:35 +0530

    adding LineSpreadMoveView class

--- linespreadmoverview/src/main/java/com/example/linespreadmoverview/LineSpreadMoveView.kt
+++ linespreadmoverview/src/main/java/com/example/linespreadmoverview/LineSpreadMoveView.kt
@@ -55,3 +55,18 @@ fun Canvas.drawLSMNode(i : Int, scale : Float, paint : Paint) {
     drawLineSpreadMove(scale, w, h, paint)
 }
 
+class LineSpreadMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
