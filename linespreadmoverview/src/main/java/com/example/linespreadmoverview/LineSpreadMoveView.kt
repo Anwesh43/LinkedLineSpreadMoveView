334ffcd62aedbae298fe36f4982d5437a05f4151
Anwesh43 <anweshthecool0@gmail.com>
2021-07-14 02:04:37 +0530

    adding State class

--- linespreadmoverview/src/main/java/com/example/linespreadmoverview/LineSpreadMoveView.kt
+++ linespreadmoverview/src/main/java/com/example/linespreadmoverview/LineSpreadMoveView.kt
@@ -24,6 +24,8 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = 30f
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -68,5 +70,25 @@ class LineSpreadMoveView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += dir * scGap
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
